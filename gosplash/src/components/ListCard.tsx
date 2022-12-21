be5fcb23bf738b2d13c543916f7b5001f933fae1
Akinori Yokoi <yokoi_akinori@arsaga.jp>
2022-12-22 00:02:17 +0900

    各写真に必要なツール類の追加

--- gosplash/src/components/ListCard.tsx
+++ gosplash/src/components/ListCard.tsx
@@ -2,6 +2,7 @@ import React, {useState} from 'react'
 import cssListCard from "../css/component/ListCard.module.css"
 import {IconButton} from "@mui/material";
 import FavoriteIcon from '@mui/icons-material/Favorite';
+import AddCircleIcon from '@mui/icons-material/AddCircle';
 
 interface Size {
     size: number
@@ -21,9 +22,13 @@ export const ListCard: React.FC<Size> = (props) => {
             onMouseLeave={() => setIsShow(false)}
         >
             <img src="#" alt="" style={widthStyle}/>
+            <p className={`${cssListCard.photographer} ${isShow? cssListCard.show : cssListCard.hidden}`}>by</p>
             <div className={`${cssListCard.reactions} ${isShow? cssListCard.show : cssListCard.hidden}`}>
                 <IconButton>
                     <FavoriteIcon />
+                </IconButton>
+                <IconButton>
+                    <AddCircleIcon />
                 </IconButton>
             </div>
         </a>
