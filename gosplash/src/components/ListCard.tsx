06621d335a2b03faa58f7e2eabd8760f8e366c93
Akinori Yokoi <yokoi_akinori@arsaga.jp>
2022-12-21 11:01:31 +0900

    ListCardにいいねボタン設置

--- gosplash/src/components/ListCard.tsx
+++ gosplash/src/components/ListCard.tsx
@@ -1,5 +1,7 @@
-import React from 'react'
+import React, {useState} from 'react'
 import cssListCard from "../css/component/ListCard.module.css"
+import {IconButton} from "@mui/material";
+import FavoriteIcon from '@mui/icons-material/Favorite';
 
 interface Size {
     size: number
@@ -9,9 +11,21 @@ export const ListCard: React.FC<Size> = (props) => {
     const widthStyle = {
         width: props.size + 'px',
     }
+
+    const [isShow, setIsShow] = useState(false)
+
     return (
-        <a className={cssListCard.wrapper}>
+        <a
+            className={cssListCard.wrapper}
+            onMouseEnter={() => setIsShow(true)}
+            onMouseLeave={() => setIsShow(false)}
+        >
             <img src="#" alt="" style={widthStyle}/>
+            <div className={`${cssListCard.reactions} ${isShow? cssListCard.show : cssListCard.hidden}`}>
+                <IconButton>
+                    <FavoriteIcon />
+                </IconButton>
+            </div>
         </a>
     )
 }
